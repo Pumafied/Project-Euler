56ecb18519e7dae626baaad488da2dc4394ac9db
Austin L <Pumafied@gmail.com>
2012-06-12 15:04:34 -0700

    Adds lots of files and thier descriptions

--- EL16.py
+++ EL16.py
@@ -1,3 +1,7 @@
+# 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+# What is the sum of the digits of the number 21000?
+
 #finds the expponent of 2^1000 and returns the sum of the numbers
 
 #Sum of it all

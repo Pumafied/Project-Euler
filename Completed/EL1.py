8d6ebfaebd7065c3db955106cdfe0607eb8ce9be
Austin <pumafied@gmail.com>
2013-01-22 10:29:43 -0600

    Completes EL1

--- Completed/EL1.py
+++ Completed/EL1.py
@@ -1,19 +1,14 @@
-#A simple script that adds up the numbers that are divisble by 3 or 5 
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
-#keeps track of the sum
-sums = 0
+#Find the sum of all the multiples of 3 or 5 below 1000.
 
-#keeps track of the programs place 
-PC = 0
 
+PC = 1
+add = 0
 while PC < 1000:
-	if PC % 5 == 0:
-		#adds up the the modulous 5 and modulous 3 
-		sums = PC + sums 
-	elif PC % 3 == 0:
-                #adds up the the modulous 5 and modulous 3 
-		sums = PC + sums
-	#counts the program up
-	PC = PC + 1
-#returns the answer
-print sums
+	if PC%5 == 0:
+		add = add + PC;
+	elif PC%3 == 0:
+		add = add + PC;		
+	PC += 1;
+print add;

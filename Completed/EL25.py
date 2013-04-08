6d08a68f142e3b106940c2b4b817ff7daebad1ac
Austin <pumafied@Devtop.(none)>
2013-04-07 22:21:14 -0500

    Completes two of the problems

--- Completed/EL25.py
+++ Completed/EL25.py
@@ -18,3 +18,14 @@
 # The 12th term, F12, is the first term to contain three digits.
 
 # What is the first term in the Fibonacci sequence to contain 1000 digits?
+
+a = 1
+b = 2
+PC = 2
+
+while len(str(a)) < 1000:
+    c = a + b
+    b = a
+    a = c
+    PC += 1
+print PC
--- working/EL20.py
+++ working/EL20.py
@@ -1,57 +1,12 @@
 # For example, 10! = 10  9  ...  3  2  1 = 3628800,
 # and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
-
-# Find the sum of the digits in the number 100!
-
-
-#############End of instructions#########
-
-#keeps track of the programs loop progress
-
-PC = 0 
-
-#this is the variable that we use to return the final answer
-final = 0
-
-# this is the number that we want the factorial of
-factorial = 100
-
-#this is the sum of the actual digits
-factorialsum = 0
-
-#this is the multiplied for of the factorial
-factorialmulti = 0
-
-#this loop figures out the what the factorial of the number that we want is 
-while factorial > 0:
-	
-
-	#preforms the maintance on the loop
-	factorial = factorial - 1
-# this loop adds the digits of that number together for our final answer
-
-factorialTopLimit = 100
-factorialBottomLimit = 1
-factorial_sum = 0
-ProgramCounter = 0
-Spliter1 = 0;
-Spliter2 = 1;
-#loop over the values under 100 down to 0
-while factorialTopLimit > 0
-	factorial_
-
-
-while factorial > 0: 
-	factorial_base = factorial + factorial_base; 
-	factorial = factorial - 1;
-
-#count how long the number is then loop over the indvidual numbers adding them like above to factorial_sum
-Length =  len(factorial_base);
-while (PC2<Length):
-        FC_base = factorial_base[PC:PC2]
-        FC_base_sum = FC_base_sum + FC_base;
-        #Enumerates the count by one
-	Spliter1 = Spliter1 + 1;
-        Spliter2 = Spliter2 + 1;
-#Print the final answer 
-print factorial_sum
+# Find the sum of digits in !100
+factor = 10
+i = factor
+currentFact = 1
+factSum = 0
+while i > 0:
+    currentFact = currentFact * i
+    factor -= 1
+    print currentFact
+print factSum
